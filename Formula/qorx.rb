12e61fbe2d5e10e061194f3c5a4c287f401d761a
Marvin Villanueva <maintainers@qorx.local>
2026-05-01 16:10:10 +0800

    Add Qorx formula

---
+++ Formula/qorx.rb
@@ -0,0 +1,17 @@
+class Qorx < Formula
+  desc "Language and runtime for local context resolution"
+  homepage "https://github.com/bbrainfuckk/qorx"
+  url "https://github.com/bbrainfuckk/qorx.git",
+      tag:      "v1.0.1"
+  license "AGPL-3.0-only"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--path", ".", "--root", prefix
+  end
+
+  test do
+    assert_match "qorx 1.0.1", shell_output("#{bin}/qorx --version")
+  end
+end

6b5fa5d59926dad948d6a67b8b8783fa08362223
Marvin Villanueva <maintainers@qorx.local>
2026-05-01 17:02:01 +0800

    Pin Qorx formula revision

--- Formula/qorx.rb
+++ Formula/qorx.rb
@@ -2,7 +2,8 @@ class Qorx < Formula
   desc "Language and runtime for local context resolution"
   homepage "https://github.com/bbrainfuckk/qorx"
   url "https://github.com/bbrainfuckk/qorx.git",
-      tag:      "v1.0.1"
+      tag:      "v1.0.1",
+      revision: "6391ff29201670fab5a55d53ab0245ddbc3f892e"
   license "AGPL-3.0-only"
 
   depends_on "rust" => :build

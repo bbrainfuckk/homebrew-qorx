49ec91ea1483339da3ea51f3665e4c0e0df3446a
Marvin Villanueva <marvin@users.noreply.github.com>
2026-05-02 13:41:40 +0800

    Update qorx formula to v1.0.3

--- Formula/qorx.rb
+++ Formula/qorx.rb
@@ -2,8 +2,7 @@ class Qorx < Formula
   desc "Language and runtime for local context resolution"
   homepage "https://github.com/bbrainfuckk/qorx"
   url "https://github.com/bbrainfuckk/qorx.git",
-      tag:      "v1.0.2",
-      revision: "86b7a40ba7f5bafa67cd3a15367fc35c0cc6d760"
+      tag:      "v1.0.3"
   license "AGPL-3.0-only"
 
   depends_on "rust" => :build
@@ -13,6 +12,6 @@ class Qorx < Formula
   end
 
   test do
-    assert_match "qorx 1.0.2", shell_output("#{bin}/qorx --version")
+    assert_match "qorx 1.0.3", shell_output("#{bin}/qorx --version")
   end
 end
